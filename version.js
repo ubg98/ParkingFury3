f0b582995e4a4bd16092538eb5bbcf0e806e00f8
games235 <games235@skm.vn>
2023-10-24 08:56:46 +0000

    update

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
-version="cc2";
-libs=["js/phaser/phaser-ads.min.js","js/phaser/phaser-cachebuster.min.js","js/phaser/phaser-super-storage.min.js","js/phaser/phaser-spine.min.js","js/phaser/splash.min.js"];
+version="5";
+libs=["js/phaser/phaser-cachebuster.min.js","js/phaser/phaser-spine.min.js","js/phaser/phaser-super-storage.min.js"];
